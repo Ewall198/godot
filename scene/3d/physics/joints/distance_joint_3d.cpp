5f2a9c0c4b16377860a04c8a4bb080c6a0c66de1
Evan <Ewall198@gmail.com>
2026-02-22 22:15:43 -0500

    DistanceJoint - hide slider for stiffness and damping

--- scene/3d/physics/joints/distance_joint_3d.cpp
+++ scene/3d/physics/joints/distance_joint_3d.cpp
@@ -37,8 +37,8 @@ void DistanceJoint3D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_point_param", "point"), &DistanceJoint3D::get_point_param);
 	ClassDB::bind_method(D_METHOD("get_global_point", "point"), &DistanceJoint3D::get_global_point);
 
-	ADD_PROPERTYI(PropertyInfo(Variant::FLOAT, "spring/stiffness", PROPERTY_HINT_RANGE, "0,100,or_greater,suffix:N/m"), "set_param", "get_param", PARAM_LIMITS_SPRING_STIFFNESS);
-	ADD_PROPERTYI(PropertyInfo(Variant::FLOAT, "spring/damping", PROPERTY_HINT_RANGE, "0,2,or_greater"), "set_param", "get_param", PARAM_LIMITS_SPRING_DAMPING);
+	ADD_PROPERTYI(PropertyInfo(Variant::FLOAT, "spring/stiffness", PROPERTY_HINT_RANGE, "0,100,or_greater,hide_control,suffix:N/m"), "set_param", "get_param", PARAM_LIMITS_SPRING_STIFFNESS);
+	ADD_PROPERTYI(PropertyInfo(Variant::FLOAT, "spring/damping", PROPERTY_HINT_RANGE, "0,2,or_greater,hide_control"), "set_param", "get_param", PARAM_LIMITS_SPRING_DAMPING);
 
 	ADD_PROPERTYI(PropertyInfo(Variant::FLOAT, "distance/min", PROPERTY_HINT_RANGE, "0,100,or_greater,suffix:m"), "set_param", "get_param", PARAM_DISTANCE_MIN);
 	ADD_PROPERTYI(PropertyInfo(Variant::FLOAT, "distance/max", PROPERTY_HINT_RANGE, "0,100,or_greater,suffix:m"), "set_param", "get_param", PARAM_DISTANCE_MAX);
